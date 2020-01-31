e7b6c3b57ce06d50d791444d3e7e70884e7152f7
rochaeduardo <eduardo.rocha2510@hotmail.com>
2020-01-31 12:51:47 -0300

    Inicializando componentes da interface

--- app/src/main/java/com/erocha/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/erocha/alcoolougasolina/MainActivity.java
@@ -3,12 +3,20 @@ package com.erocha.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+        private EditText editTextAlcool, editTextGasolina;
+        private TextView textResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextAlcool          = findViewById(R.id.editTextAlcool);
+        editTextGasolina        = findViewById(R.id.editTextGasolina);
+        textResultado           = findViewById(R.id.textResultado);
     }
 }
